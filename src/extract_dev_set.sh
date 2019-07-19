9c282d584c33061c727be0cf5462702ba4df98a8
Deng Yao <torshie@gmail.com>
2019-07-19 09:51:15 +0800

    Fixed dev set extraction error.

--- src/extract_dev_set.sh
+++ src/extract_dev_set.sh
@@ -8,7 +8,7 @@ cd $(dirname $0)/../data
 
 tar xfvz dev.tgz \
 	dev/newsdev2017-enzh-src.en.sgm \
-	dev/newstest2017-zhen-src.zh.sgm
+	dev/newsdev2017-enzh-ref.zh.sgm
 
 ../src/xml_to_text.sh < dev/newsdev2017-enzh-src.en.sgm > dev/dev.src
 ../src/xml_to_text.sh < dev/newsdev2017-enzh-ref.zh.sgm > dev/dev.tgt
